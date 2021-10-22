aa8e95f225f263ef89581d91e03ad1379251f5a7
Caiovlima2 <43886910+Caiovlima2@users.noreply.github.com>
2021-10-22 19:56:08 -0300

    Add files via upload

---
+++ APS/src/salvar.php
@@ -0,0 +1,10 @@
+<?php
+    if ($_POST['latitude'] != null && $_POST['longitude'] != null && $_POST['endereco'] != null):
+        $f = fopen("../dados/dados.txt", "a+", 0);
+        $linha = $_POST['latitude'] . ", " . $_POST['longitude'] . " : " . $_POST['endereco'] . "\n";
+        fwrite($f, $linha, strlen($linha)); 
+        fclose($f);
+        echo "<script>window.location='../index.php';</script>";
+    else:
+        echo "<script>window.location='../index.php?null=true';</script>";     
+    endif;
