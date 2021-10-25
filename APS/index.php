dbd8c0f0f79a6dc1a0090581bcfb054e25fadc5b
Caiovlima2 <43886910+Caiovlima2@users.noreply.github.com>
2021-10-25 20:34:24 -0300

    Update index.php

--- APS/index.php
+++ APS/index.php
@@ -67,11 +67,11 @@
                 <div class="modal-dialog modal-sm">
                     <div class=modal-content>
                         <div class=modal-header>
-                            <button type=button class=close data-dismiss=modal aria-label=Close><span ariahidden=true>×</span></button>
+                            <button type=button class=close data-dismiss=modal aria-label=Close><span aria-hidden=true>×</span></button>
                                 <h4 class=modal-title id=mySmallModalLabel>Mapa de Queimadas</h4>
                         </div>
                         <div class=modal-body>
-                            <p id=msg>teste</p>
+                            <p id=msg></p>
                         </div>
                     </div>
                 </div>
