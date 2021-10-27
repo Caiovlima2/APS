8929867ad4f7ba603bbcb2add4924ce5e89febd3
Caiovlima2 <43886910+Caiovlima2@users.noreply.github.com>
2021-10-27 16:47:53 -0300

    Update index.php

--- APS/index.php
+++ APS/index.php
@@ -1,31 +1,45 @@
-<!DOCTYPE html>
-<html>
-  <head>
-    <title>Simple Map</title>
-    <script src="https://polyfill.io/v3/polyfill.min.js?features=default"></script>
-    <link rel="stylesheet" type="text/css" href="./style.css" />
-    <script src="./index.js"></script>
-  </head>
-  <body>
-    <div id="map"></div>
+<?php
+    require_once('src/geraxml.php'); 
+    require_once('src/Mobile_Detect.php');
+    $detect = new Mobile_Detect;
+?>
+<!doctype html>
+<html lang=pt-BR>
+    <head>
+        <meta name=robots content="noindex, nofollow"/>
+        <link rel=icon href=forest-fire.png>
+        <meta charset=UTF-8>
+        <meta http-equiv=X-UA-Compatible content="IE=edge">
+        <meta name=viewport content="width=device-width, initial-scale=1">
+        <title>Mapa de Queimadas</title>
+        <meta name=description content="APS"/>
+        <link href=css/bootstrap.min.css rel=stylesheet>
+        <link href=css/mapa.css>
+        <script>
+          
+          let map;
 
-    <!-- Async script executes immediately and must be after any DOM elements used in callback. -->
-    <script
-      src="https://maps.googleapis.com/maps/api/js?key=AIzaSyA_Vw0vepcXiM2vw2gE7UNpu3PFOTUPEEg&callback=initMap&v=weekly"
-      async
-    ></script>
+            function initMap() {
+              map = new google.maps.Map(document.getElementById("map"), {
+                center: { lat: -34.397, lng: 150.644 },
+                zoom: 8,
+              });
+            }
+          
+        </script>
+    </head>
+    <body>
+        <?php if ($detect->isMobile()):?>
+        <div id="map"></div>
+
+        <!-- Async script executes immediately and must be after any DOM elements used in callback. -->
+        <script
+          src="https://maps.googleapis.com/maps/api/js?key=AIzaSyA_Vw0vepcXiM2vw2gE7UNpu3PFOTUPEEg&callback=initMap&v=weekly"
+          async
+        ></script>
       
-    <script>
-        
-        let map;
-
-function initMap() {
-  map = new google.maps.Map(document.getElementById("map"), {
-    center: { lat: -34.397, lng: 150.644 },
-    zoom: 8,
-  });
-}
-        
-      </script>
-  </body>
+    </body>
 </html>
+
+
+
