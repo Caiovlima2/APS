cd2d63fa6efbd3e45c36b4bb136e2cf02bebaaa6
Caiovlima2 <43886910+Caiovlima2@users.noreply.github.com>
2021-10-27 17:02:02 -0300

    Update index.php

--- APS/index.php
+++ APS/index.php
@@ -23,6 +23,31 @@
         </div>
     </div>
 
+    <div class=container>
+    <div class="panel panel-default">
+        <div class=panel-heading>Coordenadas Geográficas</div>
+        <div class=panel-body>
+            <form action=lib/salvar.php class=col-xs-12 method=post>
+                <div class=form-group>
+                    <label for=latitude>Latitude</label>
+                    <input type=text class=form-control name=latitude id=latitude placeholder=Latitude readonly=readonly>
+                </div>
+                <div class=form-group>
+                    <label for=longitude> Longitude</label>
+                    <input type=text class=form-control name=longitude id=longitude placeholder=Longitude readonly=readonly>
+                </div>
+                <div class=form-group>
+                    <label for=longitude> Endereço</label>
+                    <input type=text class=form-control name=endereco id=endereco placeholder=Endereço readonly=readonly>
+                </div>
+                <div class=form-group>
+                    <button class="btn btn-primary getLocation"><span class="glyphicon glyphiconexclamation-sign" aria-hidden=true></span> Marcar área</button>
+                </div>
+                </form>
+            </div>
+        </div>
+    </div>
+
     <?php else: ?>
     <div class=container>
         <div class="col-lg-12 col-md-12 page-header">
