7b9f0e8371c6c610c7888b1891ca3e7d84d7ca8a
Caiovlima2 <43886910+Caiovlima2@users.noreply.github.com>
2021-10-27 16:44:45 -0300

    Update index.php

--- APS/index.php
+++ APS/index.php
@@ -1,193 +1,31 @@
-<?php
-    require_once('src/geraxml.php'); 
-    require_once('src/Mobile_Detect.php');
-    $detect = new Mobile_Detect;
-?>
-<!doctype html>
-<html lang=pt-BR>
-    <head>
-        <meta name=robots content="noindex, nofollow"/>
-        <link rel=icon href=forest-fire.png>
-        <meta charset=UTF-8>
-        <meta http-equiv=X-UA-Compatible content="IE=edge">
-        <meta name=viewport content="width=device-width, initial-scale=1">
-        <title>Mapa de Queimadas</title>
-        <script src="https://polyfill.io/v3/polyfill.min.js?features=default"></script>
-        <meta name=description content="APS"/>
-        <link href=css/bootstrap.min.css rel=stylesheet>
-        <link href=css/mapa.css rel=stylesheet>
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Simple Map</title>
+    <script src="https://polyfill.io/v3/polyfill.min.js?features=default"></script>
+    <link rel="stylesheet" type="text/css" href="./style.css" />
+    <script src="./index.js"></script>
+  </head>
+  <body>
+    <div id="map"></div>
+
+    <!-- Async script executes immediately and must be after any DOM elements used in callback. -->
+    <script
+      src="https://maps.googleapis.com/maps/api/js?key=YOUR_API_KEY&callback=initMap&v=weekly"
+      async
+    ></script>
+      
+    <script>
         
-       
-    </head>
-    <body>
-        <?php if ($detect->isMobile()):?>
-            <div class=container>
-                <div class="col-lg-12 col-md-12 page-header">
-                    <h1><strong>Mapa de Queimadas</strong></h1>
-                </div>
-            </div>
-            
-            <div class=container>
-                <div class="panel panel-default">
-                    <div class=panel-heading>Coordenadas Geográficas</div>
-                    <div class=panel-body>
-                        <form action=lib/salvar.php class=col-xs-12 method=post>
-                            <div class=form-group>
-                                <label for=latitude>Latitude</label>
-                                <input type=text class=form-control name=latitude id=latitude placeholder=Latitude readonly=readonly>
-                            </div>
-                            <div class=form-group>
-                                <label for=longitude> Longitude</label>
-                                <input type=text class=form-control name=longitude id=longitude placeholder=Longitude readonly=readonly>
-                            </div>
-                            <div class=form-group>
-                                <label for=longitude> Endereço</label>
-                                <input type=text class=form-control name=endereco id=endereco placeholder=Endereço readonly=readonly>
-                            </div>
-                            <div class=form-group>
-                                <button class="btn btn-primary getLocation"><span class="glyphicon glyphiconexclamation-sign" aria-hidden=true></span> Marcar área</button>
-                            </div>
-                        </form>
-                    </div>
-                </div>
-            </div>
-            
-            <div class=container>
-                <div class="panel panel-default">
-                    <div class=panel-heading>Mapa das Queimadas</div>
-                    <div class=panel-body>
-                        <p class=help-block>Pontos de Queimadas: <span id=pontos class=badge></span></p>
-                        <div id=map class=col-xs-12></div>
-                    </div>
-                </div>
-            </div>
+        let map;
 
-            <div id=myModal class="modal fade bs-example-modal-sm" tabindex=-1 role=dialog aria-labelledby=mySmallModalLabel>
-                <div class="modal-dialog modal-sm">
-                    <div class=modal-content>
-                        <div class=modal-header>
-                            <button type=button class=close data-dismiss=modal aria-label=Close><span aria-hidden=true>×</span></button>
-                                <h4 class=modal-title id=mySmallModalLabel>Mapa de Queimadas</h4>
-                        </div>
-                        <div class=modal-body>
-                            <p id=msg></p>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <div id=map class=col-xs-12></div>
-            <script
-            src="https://maps.googleapis.com/maps/api/js?key=AIzaSyA_Vw0vepcXiM2vw2gE7UNpu3PFOTUPEEg&callback=initMap&v=weekly"
-            async
-            ></script>
-            
-            <script src=js/bootstrap.min.js></script>
-            
-            <?php if (isset($_GET['null']) == true): ?>
-                <script>
-                    $(window).load(function(){
-                        modal.innerHTML = "Não conseguimos obter suas coordenadas, tente novamente.";
-                        $("#myModal").modal("show")
-                    }) 
-                </script>
-            <?php endif; ?>
-
-            <script>
-
-                let map;
-
-                function initMap() {
-                map = new google.maps.Map(document.getElementById("map"), {
-                    center: { lat: -34.397, lng: 150.644 },
-                    zoom: 8,
-                });
-                }               
-
-                $(document).ready(function(){
-                    var h = new google.maps.Geocoder;
-                    modal = document.getElementById("msg");
-                    latitude = document.getElementById("latitude");
-                    longitude = document.getElementById("longitude");
-                    endereco = document.getElementById("endereco");
-                    pontos = document.getElementById("pontos");
-                    if(navigator.geolocation){
-                        navigator.geolocation.getCurrentPosition(d)
-                    }else{
-                        modal.innerHTML="O seu navegador não suporta Geolocalização.";
-                        $("#myModal").modal("show")
-                    }
-                    function d(i){
-                        var j = new google.maps.LatLng(i.coords.latitude,i.coords.longitude);
-                        latitude.value = i.coords.latitude;
-                        longitude.value = i.coords.longitude;
-                        b = new google.maps.Marker({ position: new google.maps.LatLng(parseFloat(i.coords.latitude), parseFloat(i.coords.longitude)), map:g});
-                        google.maps.event.addListener(b,"click",(function(k,l){
-                            return function(){
-                                        e.setContent("Seu local");
-                                        e.open(g,k)}})(b,c));
-                                        h.geocode({location:j},function(l,k){
-                                            if(k===google.maps.GeocoderStatus.OK){
-                                                if(l[1]) endereco.value = l[1].formatted_address;
-                                                else window.alert(" Nenhum resultado encontrado");                                                
-                                            }else{
-                                                window.alert("Falha: " + k)
-                                            }
-                                        })
-                    }
-                    var g = new google.maps.Map(document.getElementById("map"),{
-                        zoom:10,
-                        scrollwheel:false, 
-                        center: new google.maps.LatLng(-23.5483498, -46.3801577),
-                        mapTypeId:google.maps.MapTypeId.ROADMAP
-                    });
-                    var e = new google.maps.InfoWindow();
-                    var h = new google.maps.Geocoder;
-                    var b, c;
-                    var f= new XMLHttpRequest();
-                    f.onreadystatechange = function() if(f.readyState==4&&f.status==200) a(f);
-                    f.open("GET","dados/dados.xml",true);
-                    f.send();
-                    function a(j){
-                        var r = j.responseXML;
-                        var l=[];
-                        var k=[];
-                        var o=[];
-                        var p=[];
-                        var n=[];
-                        var q=[];
-                        var m=[];
-                        var i=[];
-                        
-                        for(c=0;c<r.getElementsByTagName("coordenadas").length;c++){
-                            l[c] = r.getElementsByTagName("coordenadas")[c];
-                            k[c] = l[c].childNodes[0].nodeValue;
-                            q[c] = k[c].split(":");
-                            n[c] = q[c][0].split(",");
-                            pontos.innerHTML = k.length;    
-                            o[c] = n[c][0];
-                            p[c] = n[c][1];
-                            m[c] = r.getElementsByTagName("endereco")[c];
-                            i[c] = m[c].childNodes[0].nodeValue;
-                            b = newgoogle.maps.Marker({
-                                    position: new google.maps.LatLng(parseFloat(o[c]),parseFloat(p[c])),map:g,icon:"forest-fire.png"
-                                });
-                            google.maps.event.addListener(b,"click",(function(s,t){
-                                return function(){
-                                    e.setContent("Coordenadas: "+o[t]+", "+p[t]+" <br> Endereço:"+i[t]);
-                                    e.open(g,s)
-                                }
-                            })(b,c))
-                        }
-                    }
-                });
-            </script>
-            
-        <?php else: ?>
-            <div class=container>
-                <div class="col-lg-12 col-md-12 page-header">
-                    <h1 class=text><strong>Este sistema funciona apenas em dispositivo móvel.</strong></h1>
-                </div>
-            </div>
-        <?php endif ;?>
-    </body>
+function initMap() {
+  map = new google.maps.Map(document.getElementById("map"), {
+    center: { lat: -34.397, lng: 150.644 },
+    zoom: 8,
+  });
+}
+        
+      </script>
+  </body>
 </html>
