e6121978e349897aa4438c6f9d94ac4283e45ada
Caiovlima2 <43886910+Caiovlima2@users.noreply.github.com>
2021-10-27 23:06:36 -0300

    Update index.php

--- APS/index.php
+++ APS/index.php
@@ -51,7 +51,7 @@
                     <h2><strong>Mapa de Queimadas</strong></h2>
                 </div>
             </div>
-        <p id=latt>teste</p>
+        <div id=latt>teste</div>
             <div class=container>
                 <div class="panel panel-default">
                     <div class=panel-heading><strong>Coordenadas Geográficas</strong></div>
