97a7c0c8861d4945fdd9d66f28097ac71a916558
Caiovlima2 <43886910+Caiovlima2@users.noreply.github.com>
2021-10-30 12:47:54 -0300

    Update salvar.php

--- APS/src/salvar.php
+++ APS/src/salvar.php
@@ -7,21 +7,44 @@ $long = $_POST['longitude'];
 $end = $_POST['endereco'];      
 
 
+       if(empty($lat){
+       
+              echo "<script type='javascript'>alert('Latitude Vazia');";
+              echo "<script>window.location='../index.php';</script>";
+              
+       }elseif(empty($long){
+       
+              echo "<script type='javascript'>alert('Longitude Vazia');";
+              echo "<script>window.location='../index.php';</script>";
+              
+       }elseif(empty($end){
+       
+              echo "<script type='javascript'>alert('Endereço Vazio');";
+              echo "<script>window.location='../index.php';</script>";
+              
+       }else{
+              
+              echo $lat;
+              echo $long;
+              echo $end;
+       
+       }
 
-       if ($lat != null && $_long != null && $end != null){
-          echo $lat;
-          echo $long;
-          echo $end;
+
+//        if ($lat != null && $_long != null && $end != null){
+//           echo $lat;
+//           echo $long;
+//           echo $end;
 //          $f = fopen("../dados/dados.txt", "a+", 0);
 //          $linha = $_POST['latitude'] . ", " . $_POST['longitude'] . " : " . $_POST['endereco'] . "\n";
 //          fwrite($f, $linha, strlen($linha)); 
 //          fclose($f);
 //         echo "<script>alert("Deu certo!");</script>"     
 //          echo "<script>window.location='../index.php';</script>";
-       }else{
-         echo "Vazio";
+//        }else{
+//          echo "Vazio";
 //          echo "<script>alert("Deu errado!");</script>" 
 //           echo "<script>window.location='../index.php?null=true';</script>"; 
              
-       }
+//        }
 ?>
