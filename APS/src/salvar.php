948844a8d4fd7f84d8b2ca4bb8b454572cdff9ef
Caiovlima2 <43886910+Caiovlima2@users.noreply.github.com>
2021-10-30 13:02:18 -0300

    Update salvar.php

--- APS/src/salvar.php
+++ APS/src/salvar.php
@@ -24,33 +24,15 @@ $end = $_POST['endereco'];
               
        }else{
               
-//               echo $lat;
-//               echo $long;
-//               echo $end;
               $f = fopen("../dados/dados.txt", "a+", 0);
               $linha = $lat . ", " . $long . " : " . $end . "\n";
               echo $linha;
               fwrite($f, $linha, strlen($linha)); 
               fclose($f);
               echo "<script type='text/javascript'>alert('Área Marcada');</script>";     
-//               echo "<script>window.location='../index.php';</script>";
+               echo "<script>window.location='../index.php';</script>";
        }
 
 
-//        if ($lat != null && $_long != null && $end != null){
-//           echo $lat;
-//           echo $long;
-//           echo $end;
-//          $f = fopen("../dados/dados.txt", "a+", 0);
-//          $linha = $_POST['latitude'] . ", " . $_POST['longitude'] . " : " . $_POST['endereco'] . "\n";
-//          fwrite($f, $linha, strlen($linha)); 
-//          fclose($f);
-//         echo "<script>alert("Deu certo!");</script>"     
-//          echo "<script>window.location='../index.php';</script>";
-//        }else{
-//          echo "Vazio";
-//          echo "<script>alert("Deu errado!");</script>" 
-//           echo "<script>window.location='../index.php?null=true';</script>"; 
-             
-//        }
+
 ?>
