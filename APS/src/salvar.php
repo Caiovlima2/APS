dc9d9eb1d0197c20666d971a3d281498653e2cdf
Caiovlima2 <43886910+Caiovlima2@users.noreply.github.com>
2021-10-30 12:02:34 -0300

    Update salvar.php

--- APS/src/salvar.php
+++ APS/src/salvar.php
@@ -1,10 +1,12 @@
 <?php
-//     if ($_POST['latitude'] != null && $_POST['longitude'] != null && $_POST['endereco'] != null){
+     if ($_POST['latitude'] != null && $_POST['longitude'] != null && $_POST['endereco'] != null){
         $f = fopen("../dados/dados.txt", "a+", 0);
         $linha = $_POST['latitude'] . ", " . $_POST['longitude'] . " : " . $_POST['endereco'] . "\n";
         fwrite($f, $linha, strlen($linha)); 
         fclose($f);
-        echo "<script>window.location='../index.php';</script>";
-//     }else{
-//         echo "<script>window.location='../index.php?null=true';</script>";     
-//     }
+        echo "<script>alert("Deu certo!");</script>"     
+//        echo "<script>window.location='../index.php';</script>";
+     }else{
+//         echo "<script>window.location='../index.php?null=true';</script>"; 
+           echo "<script>alert("Deu errado!");</script>"     
+     }
