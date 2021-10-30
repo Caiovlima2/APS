246f3ac90fc041b45cb20c60ad435d66cce1b982
Caiovlima2 <43886910+Caiovlima2@users.noreply.github.com>
2021-10-30 12:37:38 -0300

    Update salvar.php

--- APS/src/salvar.php
+++ APS/src/salvar.php
@@ -1,24 +1,17 @@
 <?php
 
-echo "Teste";
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-  // collect value of input field
-  $name = $_POST['latitude'];
-  if (empty($name)) {
-    echo "Name is empty";
-  } else {
-    echo $name;
-  }
-}
-//      if ($_POST['latitude'] != null && $_POST['longitude'] != null && $_POST['endereco'] != null){
-//         $f = fopen("../dados/dados.txt", "a+", 0);
-//         $linha = $_POST['latitude'] . ", " . $_POST['longitude'] . " : " . $_POST['endereco'] . "\n";
-//         fwrite($f, $linha, strlen($linha)); 
-//         fclose($f);
-//         echo "<script>alert("Deu certo!");</script>"     
-//         echo "<script>window.location='../index.php';</script>";
-//      }else{
-//          echo "<script>window.location='../index.php?null=true';</script>"; 
-//            echo "<script>alert("Deu errado!");</script>"     
-//      }
+$_SERVER["REQUEST_METHOD"] == "POST"
+
+      if ($_POST['latitude'] != null && $_POST['longitude'] != null && $_POST['endereco'] != null){
+         $f = fopen("../dados/dados.txt", "a+", 0);
+         $linha = $_POST['latitude'] . ", " . $_POST['longitude'] . " : " . $_POST['endereco'] . "\n";
+         fwrite($f, $linha, strlen($linha)); 
+         fclose($f);
+         echo "<script>alert("Deu certo!");</script>"     
+         echo "<script>window.location='../index.php';</script>";
+      }else{
+          echo "<script>alert("Deu errado!");</script>" 
+          echo "<script>window.location='../index.php?null=true';</script>"; 
+              
+      }
 ?>
